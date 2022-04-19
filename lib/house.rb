1453dfce50fc0220594c5e32a1ae64839aa33348
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-04-19 10:25:01 -0700

    halfway to shameless green. 8 tests pass

---
+++ lib/house.rb
@@ -0,0 +1,27 @@
+class House
+
+  def line(number)
+    case number
+    when 1
+      "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    when 3
+      "This is the rat that ate the malt that lay in the house that Jack built.\n"
+    when 4
+      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    when 5
+      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    when 6
+      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    when 7
+      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    when 8
+      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    end
+    
+    
+
+  end
+
+end
