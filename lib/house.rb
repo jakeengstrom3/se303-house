6fcc62c0a8d547d6dca3ea0055300c24267a8969
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-04-20 13:36:16 -0700

    move the branching logic out of the line method and into the noun_and_verb() method

--- lib/house.rb
+++ lib/house.rb
@@ -1,28 +1,7 @@
 class House
 
-  def line(number)
-    case number
-      
-      
-      when 5
-        "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 6
-        "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 7
-        "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 8
-        "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"  
-      when 9
-        "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 10
-        "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 11
-        "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      when 12
-        "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-      else
-        "This is " + number.downto(1).map {|number| new_noun_and_verb(number)}.join(' ')
-    end
+  def line(number)  
+    "This is " + number.downto(1).map {|number| new_noun_and_verb(number)}.join(' ')
   end
 
   def new_noun_and_verb(number)
@@ -35,7 +14,24 @@ class House
       "the rat that ate"
     when 4
       "the cat that killed"
+    when 5
+      "the dog that worried"
+    when 6
+      "the cow with the crumpled horn that tossed"
+    when 7
+      "the maiden all forlorn that milked"
+    when 8
+      "the man all tattered and torn that kissed"
+    when 9
+      "the priest all shaven and shorn that married"
+    when 10
+      "the rooster that crowed in the morn that woke"
+    when 11
+      "the farmer sowing his corn that kept"
+    when 12
+      "the horse and the hound and the horn that belonged to"
     end
+
   end
 
   def recite
