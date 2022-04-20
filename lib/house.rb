491cdcf33f0fcddff51169b90657048b2f4a3290
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-04-20 11:18:19 -0700

    refactor line. combine first three when statements into else clause. add new_noun_and_verb(number) function.

--- lib/house.rb
+++ lib/house.rb
@@ -2,12 +2,7 @@ class House
 
   def line(number)
     case number
-      when 1
-        "This is the house that Jack built.\n"
-      when 2
-        "This is the malt that lay in the house that Jack built.\n"
-      when 3
-        "This is the rat that ate the malt that lay in the house that Jack built.\n"
+      
       when 4
         "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
       when 5
@@ -26,6 +21,19 @@ class House
         "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
       when 12
         "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      else
+        "This is " + number.downto(1).map {|number| new_noun_and_verb(number)}.join(' ')
+    end
+  end
+
+  def new_noun_and_verb(number)
+    case number
+    when 1
+      "the house that Jack built.\n"
+    when 2
+      "the malt that lay in"
+    when 3
+      "the rat that ate"
     end
   end
 
