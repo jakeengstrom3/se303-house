e2ab49317fcfd5cc09b9a938e033d3c8bddd2f99
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-04-21 11:08:53 -0700

    remove erronious spaces. rename new_noun_and_verb to noun_and_verb

--- lib/house.rb
+++ lib/house.rb
@@ -1,16 +1,16 @@
 class House
 
   def line(number)  
-    "This is " + number.downto(1).map {|number| new_noun_and_verb(number)}.join(' ') + ".\n"
+    "This is " + number.downto(1).map {|number| noun_and_verb(number)}.join(' ') + ".\n"
   end
 
-  def new_noun_and_verb(number)
+  def noun_and_verb(number)
     "the #{noun(number)} that #{verb(number)}"
   end
 
   def noun(number)
     nouns = ["house", "malt", "rat", "cat", "dog", "cow with the crumpled horn", 
-              "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn",
+              "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", 
               "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
 
     nouns[number-1]
