596f3c389cc09f66dbb0727b9b75f7c9833e7a78
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-06-01 19:38:35 -0700

    add Clause class. add new Clause call to House.line

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,7 @@ class House
   end
 
   def line(number)  
-    article + number.downto(1).map {|number| noun_and_verb(number)}.join(' ') + ".\n"
+    article + number.downto(1).map {|number| Clause.new(number).noun_and_verb(number)}.join(' ') + ".\n"
   end
 
   def noun_and_verb(number)
@@ -29,6 +29,33 @@ class House
 
   def recite
     (1..12).each.map {|n| line(n)}.join("\n")
+  end
+
+end
+
+class Clause
+
+  def initialize(number)
+    @number = number
+  end
+
+  def noun_and_verb(number)
+    "the #{noun(number)} that #{verb(number)}"
+  end
+
+  def noun(number)
+    nouns = ["house", "malt", "rat", "cat", "dog", "cow with the crumpled horn", 
+              "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", 
+              "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
+
+    nouns[number-1]
+  end
+
+  def verb(number)
+    verbs = ["Jack built", "lay in", "ate", "killed", "worried", "tossed", "milked", 
+              "kissed", "married", "woke", "kept", "belonged to"]
+
+    verbs[number-1]
   end
 
 end
