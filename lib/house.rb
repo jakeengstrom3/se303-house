73117be999fedc088b29f2efb3da1c7fdb7a7032
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-06-01 19:41:36 -0700

    remove parameter from Clause.noun_and_verb. remove Clause methods from House

--- lib/house.rb
+++ lib/house.rb
@@ -5,26 +5,7 @@ class House
   end
 
   def line(number)  
-    article + number.downto(1).map {|number| Clause.new(number).noun_and_verb(number)}.join(' ') + ".\n"
-  end
-
-  def noun_and_verb(number)
-    "the #{noun(number)} that #{verb(number)}"
-  end
-
-  def noun(number)
-    nouns = ["house", "malt", "rat", "cat", "dog", "cow with the crumpled horn", 
-              "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn", 
-              "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"]
-
-    nouns[number-1]
-  end
-
-  def verb(number)
-    verbs = ["Jack built", "lay in", "ate", "killed", "worried", "tossed", "milked", 
-              "kissed", "married", "woke", "kept", "belonged to"]
-
-    verbs[number-1]
+    article + number.downto(1).map {|number| Clause.new(number).noun_and_verb}.join(' ') + ".\n"
   end
 
   def recite
@@ -39,8 +20,8 @@ class Clause
     @number = number
   end
 
-  def noun_and_verb(number)
-    "the #{noun(number)} that #{verb(number)}"
+  def noun_and_verb()
+    "the #{noun(@number)} that #{verb(@number)}"
   end
 
   def noun(number)
